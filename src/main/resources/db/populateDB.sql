b70d8aed2e5580437850c38c01cab7fd52eef9f0
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-06-25 16:52:59 +0300

    add sql table MEALS

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,20 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
 
-INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+INSERT INTO user_roles (role, user_id)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id)
+VALUES ('2019-06-24 12:00:00', 'Завтрак', 700, 100000),
+       ('2019-06-24 11:00:00', 'Завтрак', 800, 100001),
+       ('2019-06-25 16:00:00', 'Обед', 1200, 100000),
+       ('2019-06-25 20:00:00', 'Ужин', 1500, 100000),
+       ('2019-06-26 21:00:00', 'Ужин', 1500, 100001);
