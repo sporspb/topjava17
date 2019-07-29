3c9d845479c8550915f32d5937460590acc7c8c8
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-07-29 16:07:43 +0300

    7 05 fix hint graph

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -13,10 +13,10 @@ import static ru.javawebinar.topjava.UserTestData.*;
 @ActiveProfiles(DATAJPA)
 class DataJpaUserServiceTest extends AbstractJpaUserServiceTest {
     @Test
-    public void testGetWithMeals() throws Exception {
-        User user = service.getWithMeals(USER_ID);
-        assertMatch(user, USER);
-        MealTestData.assertMatch(user.getMeals(), MealTestData.MEALS);
+    void testGetWithMeals() throws Exception {
+        User admin = service.getWithMeals(ADMIN_ID);
+        assertMatch(admin, ADMIN);
+        MealTestData.assertMatch(admin.getMeals(), MealTestData.ADMIN_MEAL2, MealTestData.ADMIN_MEAL1);
     }
 
     @Test(expected = NotFoundException.class)
