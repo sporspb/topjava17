760558ada8ca741fc5a57d04589d1b2b70519322
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-08-07 00:02:14 +0300

    fix add meal

--- src/main/java/ru/javawebinar/topjava/web/converter/DateTimeFormatters.java
+++ src/main/java/ru/javawebinar/topjava/web/converter/DateTimeFormatters.java
@@ -2,7 +2,9 @@ package ru.javawebinar.topjava.web.converter;
 
 import org.springframework.format.Formatter;
 
+import java.text.ParseException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -36,4 +38,19 @@ public class DateTimeFormatters {
             return lt.format(DateTimeFormatter.ISO_LOCAL_TIME);
         }
     }
+
+
+    public static class LocalDateTimeFormatter implements Formatter<LocalDateTime> {
+
+        @Override
+        public LocalDateTime parse(String text, Locale locale) throws ParseException {
+            return LocalDateTime.parse(text);
+        }
+
+        @Override
+        public String print(LocalDateTime lt, Locale locale) {
+            return lt.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        }
+    }
+
 }
