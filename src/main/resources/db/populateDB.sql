2995b93561b50c33c32cfe581449a28a68fc0c0d
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-08-20 19:28:44 +0300

    10 09 profile jsptag

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -4,12 +4,14 @@ FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES ('User', 'user@yandex.ru', 'password'),
-                                                 ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password, calories_per_day)
+VALUES ('User', 'user@yandex.ru', 'password', 2005),
+       ('Admin', 'admin@gmail.com', 'admin', 1900);
 
-INSERT INTO user_roles (role, user_id) VALUES ('ROLE_USER', 100000),
-                                              ('ROLE_ADMIN', 100001),
-                                              ('ROLE_USER', 100001);
+INSERT INTO user_roles (role, user_id)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001),
+       ('ROLE_USER', 100001);
 
 INSERT INTO meals (date_time, description, calories, user_id)
 VALUES ('2015-05-30 10:00:00', 'Завтрак', 500, 100000),
