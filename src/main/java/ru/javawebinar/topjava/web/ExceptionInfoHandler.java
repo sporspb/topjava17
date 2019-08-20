300ce09454f1c5dd29700c72828949c8421d1b21
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-08-21 00:57:00 +0300

    HW10 partially

--- src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -22,12 +23,19 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ValidationException;
 
+import static org.springframework.context.i18n.LocaleContextHolder.getLocale;
 import static ru.javawebinar.topjava.util.exception.ErrorType.*;
 
 @RestControllerAdvice(annotations = RestController.class)
 @Order(Ordered.HIGHEST_PRECEDENCE + 5)
 public class ExceptionInfoHandler {
     private static Logger log = LoggerFactory.getLogger(ExceptionInfoHandler.class);
+
+    private final ReloadableResourceBundleMessageSource bundleMessageSource;
+
+    public ExceptionInfoHandler(ReloadableResourceBundleMessageSource bundleMessageSource) {
+        this.bundleMessageSource = bundleMessageSource;
+    }
 
     //    https://stackoverflow.com/questions/538870/should-private-helper-methods-be-static-if-they-can-be-static
     private static ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException, ErrorType errorType) {
@@ -50,7 +58,12 @@ public class ExceptionInfoHandler {
     @ResponseStatus(value = HttpStatus.CONFLICT)  // 409
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ErrorInfo conflict(HttpServletRequest req, DataIntegrityViolationException e) {
-        return logAndGetErrorInfo(req, e, true, DATA_ERROR);
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        log.error(DATA_ERROR + " at request " + req.getRequestURL(), rootCause);
+        if (rootCause.toString().contains("users_unique_email_idx")) {
+            return new ErrorInfo(req.getRequestURL(), DATA_ERROR, bundleMessageSource.getMessage("valid.userForm.dublicateMail", null, getLocale()));
+        }
+        return new ErrorInfo(req.getRequestURL(), DATA_ERROR, rootCause.toString());
     }
 
     @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)  // 422
